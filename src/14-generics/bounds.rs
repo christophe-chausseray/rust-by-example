edfddd0195ff01774d53a956e689e181b16f0f7a
christophe-chausseray <chausseray.christophe@gmail.com>
2023-01-26 18:13:08 +0100

    bounds generic activity

---
+++ src/14-generics/bounds.rs
@@ -0,0 +1,66 @@
+// A trait which implements the print marker: `{:?}`.
+use std::fmt::Debug;
+
+trait HasArea {
+    fn area(&self) -> f64;
+}
+
+impl HasArea for Rectangle {
+    fn area(&self) -> f64 { self.length * self.height }
+}
+
+#[derive(Debug)]
+struct Rectangle { length: f64, height: f64 }
+#[allow(dead_code)]
+struct Triangle  { length: f64, height: f64 }
+
+// The generic `T` must implement `Debug`. Regardless
+// of the type, this will work properly.
+fn print_debug<T: Debug>(t: &T) {
+    println!("{:?}", t);
+}
+
+// `T` must implement `HasArea`. Any type which meets
+// the bound can access `HasArea`'s function `area`.
+fn area<T: HasArea>(t: &T) -> f64 { t.area() }
+
+struct Cardinal;
+struct BlueJay;
+struct Turkey;
+
+trait Red {}
+trait Blue {}
+
+impl Red for Cardinal {}
+impl Blue for BlueJay {}
+
+// These functions are only valid for types which implement these
+// traits. The fact that the traits are empty is irrelevant.
+fn red<T: Red>(_: &T)   -> &'static str { "red" }
+fn blue<T: Blue>(_: &T) -> &'static str { "blue" }
+
+fn main() {
+    let rectangle = Rectangle { length: 3.0, height: 4.0 };
+    let _triangle = Triangle  { length: 3.0, height: 4.0 };
+
+    print_debug(&rectangle);
+    println!("Area: {}", rectangle.area());
+
+    // print_debug(&_triangle);
+    // println!("Area: {}", _triangle.area());
+    // ^ TODO: Try uncommenting these.
+    // | Error: Does not implement either `Debug` or `HasArea`.
+
+    println!("Area function result : {}", area(&rectangle));
+
+    let cardinal = Cardinal;
+    let blue_jay = BlueJay;
+    let _turkey   = Turkey;
+
+    // `red()` won't work on a blue jay nor vice versa
+    // because of the bounds.
+    println!("A cardinal is {}", red(&cardinal));
+    println!("A blue jay is {}", blue(&blue_jay));
+    // println!("A turkey is {}", red(&_turkey));
+    // ^ TODO: Try uncommenting this line.
+}
