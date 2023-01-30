04bc8a8db73c68ad297ed8fad7bd799025482d9a
christophe-chausseray <chausseray.christophe@gmail.com>
2023-01-30 09:33:55 +0100

    scoping raii activity

---
+++ src/15-scoping-rules/raii.rs
@@ -0,0 +1,34 @@
+// Scopes indicate to the compiler when borrows are valid, when resources can be freed, and when variables are created or destroyed.
+
+// Rust enforces RAII (Resource Acquisition Is Initialization), so whenever an object goes out of scope, its destructor is called and its owned resources are freed.
+
+// This behavior shields against resource leak bugs, so you'll never have to manually free memory or worry about memory leaks again!
+
+fn create_box() {
+    // Allocate an integer on the heap
+    let _box1 = Box::new(3i32);
+
+    // `_box1` is destroyed here, and memory gets freed
+}
+
+fn main() {
+    // Allocate an integer on the heap
+    let _box2 = Box::new(5i32);
+
+    // A nested scope:
+    {
+        // Allocate an integer on the heap
+        let _box3 = Box::new(4i32);
+
+        // `_box3` is destroyed here, and memory gets freed
+    }
+
+    // Creating lots of boxes just for fun
+    // There's no need to manually free memory!
+    for _ in 0u32..1_000 {
+        create_box();
+    }
+
+    // `_box2` is destroyed here, and memory gets freed
+}
+
