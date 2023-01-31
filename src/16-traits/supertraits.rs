74b32eae2f79ab29bf4f79802e7d13f1c88f0d3a
christophe-chausseray <chausseray.christophe@gmail.com>
2023-01-31 17:12:45 +0100

    supertraits activity

---
+++ src/16-traits/supertraits.rs
@@ -0,0 +1,73 @@
+// Rust doesn't have "inheritance", but you can define a trait as being a superset of another trait.
+trait Person {
+    fn name(&self) -> String;
+}
+
+// Person is a supertrait of Student.
+// Implementing Student requires you to also impl Person.
+trait Student: Person {
+    fn university(&self) -> String;
+}
+
+trait Programmer {
+    fn fav_language(&self) -> String;
+}
+
+// CompSciStudent (computer science student) is a subtrait of both Programmer
+// and Student. Implementing CompSciStudent requires you to impl both supertraits.
+trait CompSciStudent: Programmer + Student {
+    fn git_username(&self) -> String;
+}
+
+
+struct CompSciStudentType {
+  name: &'static str,
+  university: &'static str,
+  fav_language: &'static str,
+  git_username: &'static str
+}
+
+impl CompSciStudent for CompSciStudentType {
+  fn git_username(&self) -> String {
+    self.git_username.to_string()
+  }
+}
+
+impl Programmer for CompSciStudentType {
+  fn fav_language(&self) -> String {
+    self.fav_language.to_string()
+  }
+}
+
+impl Student for CompSciStudentType {
+  fn university(&self) -> String {
+    self.university.to_string()
+  }
+}
+
+impl Person for CompSciStudentType {
+  fn name(&self) -> String {
+    self.name.to_string()
+  }
+}
+
+fn comp_sci_student_greeting(student: &dyn CompSciStudent) -> String {
+    format!(
+        "My name is {} and I attend {}. My favorite language is {}. My Git username is {}",
+        student.name(),
+        student.university(),
+        student.fav_language(),
+        student.git_username()
+    )
+}
+
+fn main() {
+  let student = CompSciStudentType {
+    name: "Christophe",
+    university: "LR University",
+    fav_language: "Rust",
+    git_username: "Chris"
+  };
+
+  println!("{}", comp_sci_student_greeting(&student));
+}
