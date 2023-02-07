b386e258383d1c421dfa3d490f60adcff4714e7b
christophe-chausseray <chausseray.christophe@gmail.com>
2023-02-07 16:38:37 +0100

    hashmap activity

---
+++ src/19-std-library-types/hashmap.rs
@@ -0,0 +1,119 @@
+// Where vectors store values by an integer index, HashMaps store values by key.
+// HashMap keys can be booleans, integers, strings, or any other type that implements the Eq and Hash traits.
+
+// This includes:
+
+// - bool (though not very useful since there is only two possible keys)
+// - int, uint, and all variations thereof
+// - String and &str (protip: you can have a HashMap keyed by String and call .get() with an &str)
+
+// Note that f32 and f64 do not implement Hash, likely because floating-point precision errors would make using them as hashmap keys horribly error-prone.
+
+// All collection classes implement Eq and Hash if their contained type also respectively implements Eq and Hash.
+// For example, Vec<T> will implement Hash if T implements Hash.
+
+// You can easily implement Eq and Hash for a custom type with just one line: #[derive(PartialEq, Eq, Hash)]
+
+// Like vectors, HashMaps are growable, but HashMaps can also shrink themselves when they have excess space.
+// You can create a HashMap with a certain starting capacity using HashMap::with_capacity(uint),
+// or use HashMap::new() to get a HashMap with a default initial capacity (recommended).
+
+
+use std::collections::HashMap;
+
+// Eq requires that you derive PartialEq on the type.
+#[derive(PartialEq, Eq, Hash)]
+struct Account<'a>{
+    username: &'a str,
+    password: &'a str,
+}
+
+struct AccountInfo<'a>{
+    name: &'a str,
+    email: &'a str,
+}
+
+type Accounts<'a> = HashMap<Account<'a>, AccountInfo<'a>>;
+
+fn call(number: &str) -> &str {
+    match number {
+        "798-1364" => "We're sorry, the call cannot be completed as dialed.
+            Please hang up and try again.",
+        "645-7689" => "Hello, this is Mr. Awesome's Pizza. My name is Fred.
+            What can I get for you today?",
+        _ => "Hi! Who is this again?"
+    }
+}
+
+fn try_logon<'a>(accounts: &Accounts<'a>,
+        username: &'a str, password: &'a str){
+    println!("Username: {}", username);
+    println!("Password: {}", password);
+    println!("Attempting logon...");
+
+    let logon = Account {
+        username,
+        password,
+    };
+
+    match accounts.get(&logon) {
+        Some(account_info) => {
+            println!("Successful logon!");
+            println!("Name: {}", account_info.name);
+            println!("Email: {}", account_info.email);
+        },
+        _ => println!("Login failed!"),
+    }
+}
+
+fn main() {
+    let mut contacts = HashMap::new();
+
+    contacts.insert("Daniel", "798-1364");
+    contacts.insert("Ashley", "645-7689");
+    contacts.insert("Katie", "435-8291");
+    contacts.insert("Robert", "956-1745");
+
+    // Takes a reference and returns Option<&V>
+    match contacts.get(&"Daniel") {
+        Some(&number) => println!("Calling Daniel: {}", call(number)),
+        _ => println!("Don't have Daniel's number."),
+    }
+
+    // `HashMap::insert()` returns `None`
+    // if the inserted value is new, `Some(value)` otherwise
+    contacts.insert("Daniel", "164-6743");
+
+    match contacts.get(&"Ashley") {
+        Some(&number) => println!("Calling Ashley: {}", call(number)),
+        _ => println!("Don't have Ashley's number."),
+    }
+
+    contacts.remove(&"Ashley");
+
+    // `HashMap::iter()` returns an iterator that yields
+    // (&'a key, &'a value) pairs in arbitrary order.
+    for (contact, &number) in contacts.iter() {
+        println!("Calling {}: {}", contact, call(number));
+    }
+
+    println!("Contacts : {:?}", contacts);
+
+    let mut accounts: Accounts = HashMap::new();
+
+    let account = Account {
+        username: "j.everyman",
+        password: "password123",
+    };
+
+    let account_info = AccountInfo {
+        name: "John Everyman",
+        email: "j.everyman@email.com",
+    };
+
+    accounts.insert(account, account_info);
+
+    try_logon(&accounts, "j.everyman", "wrongPassword");
+
+    try_logon(&accounts, "j.everyman", "password123");
+}
