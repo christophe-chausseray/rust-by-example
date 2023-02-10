e2fc47f16ad40854fcb8bb5b2a27969cab7dd9e7
christophe-chausseray <chausseray.christophe@gmail.com>
2023-02-10 16:47:30 +0100

    playground activity

---
+++ src/24-meta/playground.rs
@@ -0,0 +1,10 @@
+// In mdbook, you can make code examples playable and editable
+// https://rust-lang.github.io/mdBook/
+
+// Additionally, you can add ignore if you want mdbook to skip your code when it builds and tests
+```rust,editable
+//...place your code here
+```
+fn main() {
+    println!("Hello World!");
+}
